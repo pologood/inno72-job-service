40e4740c32ec3c38fa5b8779139ed6a43ddbb560
周禹 <zhouyu@inno72.com>
2018-11-16 10:07:51 +0800

    增加 动态加载 mybatis

--- inno72-job-example/src/main/java/org/inno72/job/example/task/TaskExample.java
+++ inno72-job-example/src/main/java/org/inno72/job/example/task/TaskExample.java
@@ -1,44 +1,58 @@
 package org.inno72.job.example.task;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+import java.util.List;
+import java.util.Map;
 
 import javax.annotation.Resource;
 import javax.sql.DataSource;
 
+import org.inno72.job.example.task.mapper.Inno72ActivityMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.inno72.job.core.biz.model.ReturnT;
 import com.inno72.job.core.handle.IJobHandler;
 import com.inno72.job.core.handle.annotation.JobHandler;
+import com.inno72.job.core.handle.annotation.JobMapperScanner;
 import com.inno72.job.core.log.JobLogger;
 
+@JobMapperScanner(value = "classpath*:/org/inno72/job/example/task/mapper/*.xml", basePackage="org.inno72.job.example.task.mapper")
 @JobHandler("inno72.task.example")
 public class TaskExample implements IJobHandler
 {
 	
 	@Resource
 	DataSource ds;
+	
+	@Autowired
+	private Inno72ActivityMapper activity;
 
 	@Override
 	public ReturnT<String> execute(String param) throws Exception {
 		JobLogger.log("example job, start");
 		
-		Connection conn = ds.getConnection();
+////		Connection conn = ds.getConnection();
+////		
+////		PreparedStatement stm = conn.prepareStatement("select * from inno72.inno72_goods");
+////		
+////		ResultSet rs = stm.executeQuery();
+////		while(rs.next()) {
+////			String name = rs.getString(1);
+////			System.out.println(name);
+////		}
+//		
+//		rs.close();
+//		stm.close();
+//		conn.close();
 		
-		PreparedStatement stm = conn.prepareStatement("select * from inno72.inno72_goods");
+		List<Map<String, String>> rets = activity.queryActivityInfo(0);
 		
-		ResultSet rs = stm.executeQuery();
-		while(rs.next()) {
-			String name = rs.getString(1);
-			System.out.println(name);
+		for(Map<String, String> ret : rets) {
+			for (String key : ret.keySet()) {
+				System.out.println(String.format("{%s:%s}", key,ret.get(key)));
+			}
 		}
 		
-		rs.close();
-		stm.close();
-		conn.close();
-		
-		
-		JobLogger.log("example job, param:"+param);
+		JobLogger.log("example job, param:");
 		JobLogger.log("example job, end");
 		return new ReturnT<String>(ReturnT.SUCCESS_CODE, "ok");
 	}
--- inno72-job-local-worker/src/main/java/com/inno72/job/local/worker/config/Configure.java
+++ inno72-job-local-worker/src/main/java/com/inno72/job/local/worker/config/Configure.java
@@ -1,20 +1,36 @@
 package com.inno72.job.local.worker.config;
 
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.inno72.job.example.task.TaskExample;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.mapper.MapperScannerConfigurer;
 import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.core.io.support.ResourcePatternResolver;
 
 import com.inno72.job.core.handle.IJobHandler;
-import com.inno72.job.focus.count.FocusCountTask;
+import com.inno72.job.core.handle.annotation.JobMapperScanner;
 
 @Configuration
 @Import({RedisAutoConfiguration.class, MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 public class Configure {
+	
+	static final public Class<? extends IJobHandler> TaskClazz = TaskExample.class;
 	
 	@Configuration
     @Profile("dev")
@@ -32,10 +48,50 @@ public class Configure {
     	
     }
     
+    @Bean
+    @Conditional(MybatisCondition.class)
+	public SqlSessionFactory sqlSessionFactoryBean(DataSource dataSource) throws IOException {
+		
+		JobMapperScanner jobMapperScanner = TaskClazz.getAnnotation(JobMapperScanner.class);
+		SqlSessionFactoryBean factory = new SqlSessionFactoryBean();
+		
+		factory.setDataSource(dataSource);
+		
+		ResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+		
+		List<org.springframework.core.io.Resource> resourceList = new LinkedList<org.springframework.core.io.Resource>();
+		
+		for(String value : jobMapperScanner.value()) {
+			org.springframework.core.io.Resource[] res =  resolver.getResources(value);
+			if(res != null && res.length > 0)
+				resourceList.addAll(Arrays.asList(res));
+		}
+	
+		try {
+			
+			org.springframework.core.io.Resource[] res = new org.springframework.core.io.Resource[resourceList.size()];
+			factory.setMapperLocations(resourceList.toArray(res));
+			return factory.getObject();
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new RuntimeException(e.getMessage(), e);
+		}
+	}
+    
+    @Bean
+    @Conditional(MybatisCondition.class)
+	public MapperScannerConfigurer mapperScannerConfigurer() {
+    	JobMapperScanner jobMapperScanner = TaskClazz.getAnnotation(JobMapperScanner.class);
+    	MapperScannerConfigurer mapperScannerConfigurer = new MapperScannerConfigurer();
+		mapperScannerConfigurer.setSqlSessionFactoryBeanName("sqlSessionFactoryBean");
+		mapperScannerConfigurer.setBasePackage(jobMapperScanner.basePackage());
+		return mapperScannerConfigurer;
+	}
+
     
     @Bean(name="testTask")
-    public IJobHandler jobHandler() {
-    	return new FocusCountTask();
+    public IJobHandler jobHandler() throws Exception {
+    	return TaskClazz.newInstance();
     }
 
 }
