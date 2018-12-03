bac18429b80e1654774a541e719a91d86fc3f35c
xingang <546657868@qq.com>
2018-12-03 21:12:21 +0800

    分页处理

--- inno72-job-executer-activity-count/src/main/java/com/inno72/job/executer/task/MerchantCountByDayTask.java
+++ inno72-job-executer-activity-count/src/main/java/com/inno72/job/executer/task/MerchantCountByDayTask.java
@@ -13,6 +13,7 @@ import java.util.concurrent.Executors;
 
 import javax.annotation.Resource;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.data.mongodb.core.query.Criteria;
@@ -89,170 +90,185 @@ public class MerchantCountByDayTask implements IJobHandler {
 
 		query.addCriteria(Criteria.where("serviceTime").gt(lastActionTime));
 
-		List<Inno72MachineInformation> inno72MachineInfomations = mongoOperations
-				.find(query, Inno72MachineInformation.class, "Inno72MachineInformation");
+		long total = mongoOperations
+				.count(query, Inno72MachineInformation.class, "Inno72MachineInformation");
 
-		JobLogger.log("商户日统计任务查询到待统计数据" + JSON.toJSONString(inno72MachineInfomations));
 
-		if (inno72MachineInfomations.size() == 0){
-			return new ReturnT<>(ReturnT.SUCCESS_CODE, "ok");
-		}
+		int perPage = 10000;
+		long pageSize = total / perPage + (total%perPage != 0 ? 1 : 0);
+		query.with(new Sort(Sort.Direction.ASC, "_id"));
+		query.limit(perPage);
 
-		Map<String, List<Inno72MachineInformation>> group = new HashMap<>();
-		//按日期分组
-		for (Inno72MachineInformation information : inno72MachineInfomations){
+		for (int i = 0; i < pageSize; i++) {
+			query.skip((int)(i * perPage));
 
-			String activityId = information.getActivityId();
-			String city = information.getCity();
-			String goodsId = information.getGoodsId();
-			String serviceTime = information.getServiceTime();
-			if (StringUtil.isEmpty(serviceTime)){
+			List<Inno72MachineInformation> inno72MachineInfomations = mongoOperations
+					.find(query, Inno72MachineInformation.class, "Inno72MachineInformation");
+
+			JobLogger.log("商户日统计任务查询到待统计数据" + JSON.toJSONString(inno72MachineInfomations));
+
+			if (inno72MachineInfomations.size() == 0){
 				continue;
 			}
-			String groupKey = activityId+city+goodsId+serviceTime.substring(0, 10);
 
-			List<Inno72MachineInformation> inno72MachineInformations = group.get(groupKey);
-			if (inno72MachineInformations == null){
-				inno72MachineInformations = new ArrayList<>();
-			}
-			inno72MachineInformations.add(information);
-			group.put(groupKey,inno72MachineInformations);
-		}
+			Map<String, List<Inno72MachineInformation>> group = new HashMap<>();
+			//按日期分组
+			for (Inno72MachineInformation information : inno72MachineInfomations){
 
-
-		for (Map.Entry<String, List<Inno72MachineInformation>> entry: group.entrySet()){
-
-			List<Inno72MachineInformation> value = entry.getValue();
-
-			exec.execute(() ->{
-
-				System.out.println("当前分组数据 -> " + JSON.toJSONString(value));
-
-				int stay = 0;//停留用户数
-				int concern = 0;//关注用户数
-				int gorder = 0;//订单数
-				int corder = 0;//优惠券订单数
-				int goods = 0;//出货数
-				int pay = 0;//支付数
-				int pv = 0;//pv
-				int uv = 0;
-
-				String date = "";
-				String city = "";
-				String goodsId = "";
-				String goodsName = "";
-				//inno72_merchant.merchant_code
-				String sellerId = "";
-				//inno72_merchant_user.id = inno72_merchant.merchant_account_id
-				String merchantId = "";
-
-				String activityId = "";
-				String activityName = "";
-
-				// 商户总名称 - table -> inno72_merchant_user.merchant_name =  inno72_merchant.merchant_account_name
-				String merchantName = "";// TODO 新
-				// 渠道商家ID - table -> inno72_merchant.id
-				String channelMerchantId = "";//TODO 新
-				// 渠道ID - table -> inno72_merchant.channel_id
-				String channelId = "";//TODO 新
-				// 渠道名称 - table -> inno72_merchant.channel_name
-				String channelName = "";//TODO 新
-
-				String machineCode = "";
-
-				Set<String> user = new HashSet<>();
-
-				for (Inno72MachineInformation count : value){
-
-					String type = count.getType();
-					if (StringUtil.isEmpty(date)){
-						date = count.getServiceTime().substring(0, 10);
-					}
-					if (StringUtil.isEmpty(city) && StringUtil.notEmpty(count.getCity())) {
-						city = count.getCity();
-					}
-					if (StringUtil.isEmpty(goodsId) && StringUtil.notEmpty(count.getGoodsId())) {
-						goodsId = count.getGoodsId();
-					}
-					if (StringUtil.isEmpty(goodsName) && StringUtil.notEmpty(count.getGoodsName())) {
-						goodsName = count.getGoodsName();
-					}
-					if (StringUtil.isEmpty(sellerId) && StringUtil.notEmpty(count.getSellerId())) {
-						sellerId = count.getSellerId();
-					}
-					if (StringUtil.isEmpty(activityId) && StringUtil.notEmpty(count.getActivityId())) {
-						activityId = count.getActivityId();
-					}
-					if (StringUtil.isEmpty(activityName) && StringUtil.notEmpty(count.getActivityName())) {
-						activityName = count.getActivityName();
-					}
-					//merchantAccountId
-					if (StringUtil.isEmpty(machineCode) && StringUtil.notEmpty(count.getMachineCode())) {
-						machineCode = count.getMachineCode();
-					}
-					if (StringUtil.isEmpty(merchantName) && StringUtil.notEmpty(count.getMerchantName())) {
-						merchantName = count.getMerchantName();
-					}
-					if (StringUtil.isEmpty(channelMerchantId) && StringUtil.notEmpty(count.getChannelMerchantId())) {
-						channelMerchantId = count.getChannelMerchantId();
-					}
-					if (StringUtil.isEmpty(channelId) && StringUtil.notEmpty(count.getChannelId())) {
-						channelId = count.getChannelId();
-					}
-					if (StringUtil.isEmpty(channelName) && StringUtil.notEmpty(count.getChannelName())) {
-						channelName = count.getChannelName();
-					}
-					if (StringUtil.isEmpty(merchantId) && StringUtil.notEmpty(sellerId)) {
-						merchantId = inno72MerchantTotalCountMapper.findMerchantBySellerId(sellerId);
-					}
-
-					switch (type){
-						case "100100"://停留用户数
-							stay++;
-							break;
-						case "002001"://关注
-							concern++;
-							break;
-						case "007001"://商品订单
-							gorder++;
-							break;
-						case "007002"://优惠券订单
-							corder++;
-							break;
-						case "008001"://出货
-							goods++;
-							break;
-						case "011002"://订单支付
-							pay++;
-							break;
-						case "011002 "://订单支付
-							pay++;
-							break;
-						case "001001"://登录
-							pv++;
-							user.add(count.getUserId());
-							break;
-						default:
-							System.out.println("类型不匹配的数据埋点 -> " + JSON.toJSONString(count));
-					}
-
-					System.out.println("Thread = "+ Thread.currentThread().getName() + " merchantId = "+ merchantId + " 停留用户数 = " + stay + ";关注 = " + concern
-										+ "; 商品订单 = " + gorder + "; 优惠券订单 = " + corder + "; 出货 = " + goods
-										+ "; 订单支付 = " + pay + "; pv = " + pv + "; uv = " + user.size() + "; user = " + JSON.toJSONString(user));
+				String activityId = information.getActivityId();
+				String city = information.getCity();
+				String goodsId = information.getGoodsId();
+				String serviceTime = information.getServiceTime();
+				if (StringUtil.isEmpty(serviceTime)){
+					continue;
 				}
+				String groupKey = activityId+city+goodsId+serviceTime.substring(0, 10);
 
-				Inno72MerchantTotalCountByDay day = new Inno72MerchantTotalCountByDay(
-						StringUtil.uuid(), date, city, goodsId, goodsName,
-						merchantId, gorder, pay, goods, corder,
-						concern, pv, user.size(), sellerId, LocalDateTime.now(),
-						activityId, activityName, stay, merchantName, channelMerchantId,
-						channelId, channelName, machineCode);
+				List<Inno72MachineInformation> inno72MachineInformations = group.get(groupKey);
+				if (inno72MachineInformations == null){
+					inno72MachineInformations = new ArrayList<>();
+				}
+				inno72MachineInformations.add(information);
+				group.put(groupKey,inno72MachineInformations);
+			}
 
-				inno72MerchantTotalCountByDayMapper.insert(day);
 
-				JobLogger.log("商户日统计任务插入统计数据" + JSON.toJSONString(day));
-			});
+			for (Map.Entry<String, List<Inno72MachineInformation>> entry: group.entrySet()){
+
+				List<Inno72MachineInformation> value = entry.getValue();
+
+				exec.execute(() ->{
+
+					System.out.println("当前分组数据 -> " + JSON.toJSONString(value));
+
+					int stay = 0;//停留用户数
+					int concern = 0;//关注用户数
+					int gorder = 0;//订单数
+					int corder = 0;//优惠券订单数
+					int goods = 0;//出货数
+					int pay = 0;//支付数
+					int pv = 0;//pv
+					int uv = 0;
+
+					String date = "";
+					String city = "";
+					String goodsId = "";
+					String goodsName = "";
+					//inno72_merchant.merchant_code
+					String sellerId = "";
+					//inno72_merchant_user.id = inno72_merchant.merchant_account_id
+					String merchantId = "";
+
+					String activityId = "";
+					String activityName = "";
+
+					// 商户总名称 - table -> inno72_merchant_user.merchant_name =  inno72_merchant.merchant_account_name
+					String merchantName = "";// TODO 新
+					// 渠道商家ID - table -> inno72_merchant.id
+					String channelMerchantId = "";//TODO 新
+					// 渠道ID - table -> inno72_merchant.channel_id
+					String channelId = "";//TODO 新
+					// 渠道名称 - table -> inno72_merchant.channel_name
+					String channelName = "";//TODO 新
+
+					String machineCode = "";
+
+					Set<String> user = new HashSet<>();
+
+					for (Inno72MachineInformation count : value){
+
+						String type = count.getType();
+						if (StringUtil.isEmpty(date)){
+							date = count.getServiceTime().substring(0, 10);
+						}
+						if (StringUtil.isEmpty(city) && StringUtil.notEmpty(count.getCity())) {
+							city = count.getCity();
+						}
+						if (StringUtil.isEmpty(goodsId) && StringUtil.notEmpty(count.getGoodsId())) {
+							goodsId = count.getGoodsId();
+						}
+						if (StringUtil.isEmpty(goodsName) && StringUtil.notEmpty(count.getGoodsName())) {
+							goodsName = count.getGoodsName();
+						}
+						if (StringUtil.isEmpty(sellerId) && StringUtil.notEmpty(count.getSellerId())) {
+							sellerId = count.getSellerId();
+						}
+						if (StringUtil.isEmpty(activityId) && StringUtil.notEmpty(count.getActivityId())) {
+							activityId = count.getActivityId();
+						}
+						if (StringUtil.isEmpty(activityName) && StringUtil.notEmpty(count.getActivityName())) {
+							activityName = count.getActivityName();
+						}
+						//merchantAccountId
+						if (StringUtil.isEmpty(machineCode) && StringUtil.notEmpty(count.getMachineCode())) {
+							machineCode = count.getMachineCode();
+						}
+						if (StringUtil.isEmpty(merchantName) && StringUtil.notEmpty(count.getMerchantName())) {
+							merchantName = count.getMerchantName();
+						}
+						if (StringUtil.isEmpty(channelMerchantId) && StringUtil.notEmpty(count.getChannelMerchantId())) {
+							channelMerchantId = count.getChannelMerchantId();
+						}
+						if (StringUtil.isEmpty(channelId) && StringUtil.notEmpty(count.getChannelId())) {
+							channelId = count.getChannelId();
+						}
+						if (StringUtil.isEmpty(channelName) && StringUtil.notEmpty(count.getChannelName())) {
+							channelName = count.getChannelName();
+						}
+						if (StringUtil.isEmpty(merchantId) && StringUtil.notEmpty(sellerId)) {
+							merchantId = inno72MerchantTotalCountMapper.findMerchantBySellerId(sellerId);
+						}
+
+						switch (type){
+							case "100100"://停留用户数
+								stay++;
+								break;
+							case "002001"://关注
+								concern++;
+								break;
+							case "007001"://商品订单
+								gorder++;
+								break;
+							case "007002"://优惠券订单
+								corder++;
+								break;
+							case "008001"://出货
+								goods++;
+								break;
+							case "011002"://订单支付
+								pay++;
+								break;
+							case "011002 "://订单支付
+								pay++;
+								break;
+							case "001001"://登录
+								pv++;
+								user.add(count.getUserId());
+								break;
+							default:
+								System.out.println("类型不匹配的数据埋点 -> " + JSON.toJSONString(count));
+						}
+
+						System.out.println("Thread = "+ Thread.currentThread().getName() + " merchantId = "+ merchantId + " 停留用户数 = " + stay + ";关注 = " + concern
+								+ "; 商品订单 = " + gorder + "; 优惠券订单 = " + corder + "; 出货 = " + goods
+								+ "; 订单支付 = " + pay + "; pv = " + pv + "; uv = " + user.size() + "; user = " + JSON.toJSONString(user));
+					}
+
+					Inno72MerchantTotalCountByDay day = new Inno72MerchantTotalCountByDay(
+							StringUtil.uuid(), date, city, goodsId, goodsName,
+							merchantId, gorder, pay, goods, corder,
+							concern, pv, user.size(), sellerId, LocalDateTime.now(),
+							activityId, activityName, stay, merchantName, channelMerchantId,
+							channelId, channelName, machineCode);
+
+					inno72MerchantTotalCountByDayMapper.insert(day);
+
+					JobLogger.log("商户日统计任务插入统计数据" + JSON.toJSONString(day));
+				});
+			}
 		}
+
+
 		return new ReturnT<>(ReturnT.SUCCESS_CODE, "ok");
 	}
 
