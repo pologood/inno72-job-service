0f120a0b948524ba39a6ba71f8dee2d2431c0e82
xingang <546657868@qq.com>
2019-02-21 14:40:02 +0800

    添加监控日志

--- inno72-job-executer-activity-count/src/main/java/com/inno72/job/executer/task/MerchantCountTask.java
+++ inno72-job-executer-activity-count/src/main/java/com/inno72/job/executer/task/MerchantCountTask.java
@@ -65,10 +65,6 @@ public class MerchantCountTask implements IJobHandler {
 			selectByDayParam.put("lastUpdateTime", LocalDateTimeUtil.transfer(lastUpdateTime, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
 			List<Inno72MerchantTotalCountByDay> days = inno72MerchantTotalCountByDayMapper.selectByLastTimeAndDayParam(selectByDayParam);
 
-			if ( days.size() == 0 ){
-				continue;
-			}
-
 			String activityType = count.getActivityType();
 
 			Integer machineNum;
@@ -81,14 +77,13 @@ public class MerchantCountTask implements IJobHandler {
 
 
 			Integer visitorNum = inno72MerchantTotalCountMapper.getVisitorNumFromHourLog(selectByDayParam);
-
+			count.setLastUpdateTime(LocalDateTime.now());
+			count.setVisitorNum(count.getVisitorNum() + (visitorNum == null ? 0 :visitorNum));
 			for (Inno72MerchantTotalCountByDay day : days){
 				count.setBuyer(count.getBuyer() + day.getOrderQtySucc());
 				count.setMachineNum(machineNum > count.getMachineNum() ? machineNum  :  count.getMachineNum());
 				count.setOrder(count.getOrder() + day.getOrderQtyTotal());
 				count.setShipment(count.getShipment() + day.getOrderQtySucc());
-				count.setVisitorNum(count.getVisitorNum() + (visitorNum == null ? 0 :visitorNum));
-				count.setLastUpdateTime(LocalDateTime.now());
 				count.setPv(count.getPv() + day.getPv());
 				count.setStayUser(count.getStayUser() + day.getStayNum());
 				count.setUv(count.getUv() + day.getUv());
