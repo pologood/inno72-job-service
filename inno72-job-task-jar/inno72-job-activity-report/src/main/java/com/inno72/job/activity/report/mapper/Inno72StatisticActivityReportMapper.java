19c4aa55a337950f341d9e83d771c46169f4906b
gaoxingang <546657868@qq.com>
2019-02-21 12:11:23 +0800

    按日期删除保存数据

--- inno72-job-task-jar/inno72-job-activity-report/src/main/java/com/inno72/job/activity/report/mapper/Inno72StatisticActivityReportMapper.java
+++ inno72-job-task-jar/inno72-job-activity-report/src/main/java/com/inno72/job/activity/report/mapper/Inno72StatisticActivityReportMapper.java
@@ -4,12 +4,13 @@ package com.inno72.job.activity.report.mapper;
 import com.inno72.job.activity.report.model.Inno72StatisticActivityReport;
 
 import java.util.List;
+import java.util.Map;
 
 public interface Inno72StatisticActivityReportMapper {
 
-	List<Inno72StatisticActivityReport> queryActivityReports();
+	List<Inno72StatisticActivityReport> queryActivityReports(Map params);
 
 	void insertBatch(List<Inno72StatisticActivityReport> activityReports);
 
-	void delete();
+	void delete(Map params);
 }
